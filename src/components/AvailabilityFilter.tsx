7ce876259206bfa993876ef18d020852aa5593f7
ryan woods <federer5050@gmail.com>
2023-10-28 19:04:01 +0100

    adding hovered filter - needs to go in own component

--- src/components/AvailabilityFilter.tsx
+++ src/components/AvailabilityFilter.tsx
@@ -73,7 +73,6 @@ const AvailabilityFilter = () => {
 
 
             {/* availability options */}
-
             {/* day filter */}
             <div style={{ position: 'absolute', left: '100px' }}>
                 <label>
@@ -92,107 +91,73 @@ const AvailabilityFilter = () => {
             </div>
 
 
-            {/* TESTING */}
-            <div>
-                <div style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "white",
-                    borderRadius: "20px",
-                    padding: "1rem",
-                    width: "295px"
-                }}>
-                    <div style={{
-                        display: "block",
-                        backgroundColor: "white",
-                        width: "528px",
-                        margin: "0.5rem auto 0 auto",
-                        padding: "0",
-                        alignItems: "flex-start"
-                    }}>
-                        <div style={{
-                            width: "167px",
-                            display: "none"
-                        }}>
-                            <input type="text" id="teacherAvailability" name="teacherAvailability" style={{ width: '167px', display: 'none' }} />
+            {/* PUT THE HOVERED FILTERS INTO OWN COMPONENT */}
+            {/* PUT THE HOVERED FILTERS INTO OWN COMPONENT */}
+            {/* PUT THE HOVERED FILTERS INTO OWN COMPONENT */}
+            <div className={styles.hover__wrapper}>
+                <div className={styles.hover__container}>
+                    <div style={{ width: '167px', display: 'none' }}>
+                        <input type="text" id="teacherAvailability" name="teacherAvailability" className={styles.hover__each__slot} />
+                    </div>
+                    <div>
+                        <h3 className={styles.hover__time}>Time of the day, in your time zone</h3>
+                        <div className={styles.hover__slots}>
+                            <div className={styles.hover__each__slot}>
+                                {/* Slot 1 */}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={styles.svgIcon}>
+                                    <path d="M17 18a5 5 0 0 0-10 0"></path>
+                                    <line x1="12" y1="2" x2="12" y2="9"></line>
+                                    <line x1="4.22" y1="10.22" x2="5.64" y2="11.64"></line>
+                                    <line x1="1" y1="18" x2="3" y2="18"></line>
+                                    <line x1="21" y1="18" x2="23" y2="18"></line>
+                                    <line x1="18.36" y1="11.64" x2="19.78" y2="10.22"></line>
+                                    <line x1="23" y1="22" x2="1" y2="22"></line>
+                                    <polyline points="8 6 12 2 16 6"></polyline>
+                                </svg>
+                                <span style={{ margin: '5px' }}>6-12</span>
+            Morning
+          </div>
+                            <div className={styles.hover__each__slot}>
+                                {/* Slot 2 */}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={styles.svgIcon}>
+                                    <circle cx="12" cy="12" r="5"></circle>
+                                    <line x1="12" y1="1" x2="12" y2="3"></line>
+                                    <line x1="12" y1="21" x2="12" y2="23"></line>
+                                    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                                    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                                    <line x1="1" y1="12" x2="3" y2="12"></line>
+                                    <line x1="21" y1="12" x2="23" y2="12"></line>
+                                    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                                    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                                </svg>
+                                <span style={{ margin: '5px' }}>12-5</span>
+            Afternoon
+          </div>
+                            <div className={styles.hover__each__slot}>
+                                {/* Slot 3 */}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={styles.svgIcon}>
+                                    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                                </svg>
+                                <span style={{ margin: '5px' }}>5-10</span>
+            Evening
+          </div>
                         </div>
-                        <div style={{
-                        }}>
-                            <h3 style={{ fontSize: '15px', fontWeight: 700, marginBottom: '10px' }}>Time of the day, in your time zone</h3>
-                            <div style={{
-                                display: "flex",
-                                justifyContent: "space-between"
-                            }}>
-                                <div style={{
-                                    flexDirection: "column",
-                                    display: "flex",
-                                    alignItems: "center"
-                                }} >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ width: "24px", height: "24px" }} className="feather feather-sunrise"><path d="M17 18a5 5 0 0 0-10 0"></path><line x1="12" y1="2" x2="12" y2="9"></line><line x1="4.22" y1="10.22" x2="5.64" y2="11.64"></line><line x1="1" y1="18" x2="3" y2="18"></line><line x1="21" y1="18" x2="23" y2="18"></line><line x1="18.36" y1="11.64" x2="19.78" y2="10.22"></line><line x1="23" y1="22" x2="1" y2="22"></line><polyline points="8 6 12 2 16 6"></polyline></svg>
-                                    <span style={{ margin: '5px' }} >6-12</span>
-          Morning
-        </div>
-                                <div style={{
-                                    color: '#3bb3bd',
-                                    flexDirection: "column",
-                                    display: "flex",
-                                    alignItems: "center"
-                                }} >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ width: "24px", height: "24px" }} className="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
-                                    <span style={{ margin: '5px' }} >12-5</span>
-          Afternoon
-        </div>
-                                <div style={{
-                                    color: '#3bb3bd',
-                                    flexDirection: "column",
-                                    display: "flex",
-                                    alignItems: "center"
-                                }} >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ width: "24px", height: "24px" }} className="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-                                    <span style={{ margin: '5px' }} >5-10</span>
-          Evening
-        </div>
-                            </div>
-                            <h3 style={{ margin: '15px 0', fontSize: '15px', fontWeight: '700' }}>Days of the week</h3>
-                            <div style={{
-                                display: "flex",
-                                justifyContent: "space-between"
-                            }}>
-                                <div style={{
-                                    color: '#3bb3bd',
-                                    cursor: "pointer"
-                                }} >Mon</div>
-                                <div style={{
-                                    color: '#3bb3bd',
-                                    cursor: "pointer"
-                                }} >Tue</div>
-                                <div style={{
-                                    color: '#3bb3bd',
-                                    cursor: "pointer"
-                                }} >Wed</div>
-                                <div style={{
-                                    color: '#3bb3bd',
-                                    cursor: "pointer"
-                                }} >Thu</div>
-                                <div style={{
-                                    color: '#3bb3bd',
-                                    cursor: "pointer"
-                                }} >Fri</div>
-                                <div style={{
-                                    color: '#3bb3bd',
-                                    cursor: "pointer"
-                                }} >Sat</div>
-                                <div style={{
-                                    color: '#3bb3bd',
-                                    cursor: "pointer"
-                                }} >Sun</div>
-                            </div>
-                        </div>
+                    </div>
+                    <h3 className={styles.hover__day}>Days of the week</h3>
+                    <div className={styles.hover__days}>
+                        <div>Mon</div>
+                        <div>Tue</div>
+                        <div>Wed</div>
+                        <div>Thu</div>
+                        <div>Fri</div>
+                        <div>Sat</div>
+                        <div>Sun</div>
                     </div>
                 </div>
             </div>
-            {/* TESTING */}
+            {/* PUT THE HOVERED FILTERS INTO OWN COMPONENT */}
+            {/* PUT THE HOVERED FILTERS INTO OWN COMPONENT */}
+            {/* PUT THE HOVERED FILTERS INTO OWN COMPONENT */}
 
         </>
     )
