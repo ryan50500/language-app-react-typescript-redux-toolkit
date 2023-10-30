85e2b4563e8833691ade66011cef1dcebd1129b4
ryan woods <federer5050@gmail.com>
2023-10-30 11:09:16 +0000

    adding country cross - needs styling

--- src/components/CountryFilter.tsx
+++ src/components/CountryFilter.tsx
@@ -1,30 +1,61 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setCountry } from '../redux/FilterSlice';
-import { RootState } from '../redux/store'; // Import the RootState type
-import styles from './CountryFilter.module.css';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCountry } from "../redux/FilterSlice";
+import { RootState } from "../redux/store"; // Import the RootState type
+import styles from "./CountryFilter.module.css";
 
 const CountryFilter = () => {
-    const country = useSelector((state: RootState) => state.filters.country);
-    const dispatch = useDispatch();
+  const country = useSelector((state: RootState) => state.filters.country);
+  const dispatch = useDispatch();
 
-    // set country 
-    const handleCountryChange = (newCountry: string) => {
-        dispatch(setCountry(newCountry));
-    };
-    return (
-        <>
-            <div className={styles.country__wrapper}>
-                <div className={styles.country__content} >
-                    <h3>COUNTRY OF BIRTH</h3>
-                    <input type="text" placeholder="Search Country..." value={country} onChange={(e) => handleCountryChange(e.target.value)} />
-                </div>
-            </div>
-        </>
-    )
-}
+  const [showCountry, setShowCountry] = useState<boolean>(false);
 
-export default CountryFilter
+  // set country
+  const handleCountryChange = (newCountry: string) => {
+    setShowCountry(true);
+    dispatch(setCountry(newCountry));
+  };
 
+  // remove country filter
+  const removeCountryFilter = () => {
+    setShowCountry(false);
+    dispatch(setCountry(""));
+  };
 
+  return (
+    <>
+      <div className={styles.country__wrapper}>
+        <div className={styles.country__content}>
+          <h3>COUNTRY OF BIRTH</h3>
+          <div className={styles.country__input}>
+            <input
+              type="text"
+              placeholder="Search Country..."
+              value={country}
+              onChange={(e) => handleCountryChange(e.target.value)}
+            />
+            {/* conditionally show country cross SVG */}
+            {showCountry && (
+              <div
+                className={styles.price__cross}
+                onClick={removeCountryFilter}
+              >
+                <svg
+                  height="9"
+                  viewBox="0 0 12 12"
+                  width="9"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className={styles.price__cross__x}
+                >
+                  <path d="M6 4.586L10.293.293l1.414 1.414L7.414 6l4.293 4.293-1.414 1.414L6 7.414l-4.293 4.293-1.414-1.414L4.586 6 .293 1.707 1.707.293z"></path>
+                </svg>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
+export default CountryFilter;
